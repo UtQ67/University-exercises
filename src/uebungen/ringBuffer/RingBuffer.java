2303659b10534c582f3b1e8a2e68c98ea91c80e4
UtQ67 <pazarciutku@gmail.com>
2018-12-03 16:17:40 +0100

    Added RIngBuffer (Queue with limit)

---
+++ src/uebungen/ringBuffer/RingBuffer.java
@@ -0,0 +1,62 @@
+package uebungen.ringBuffer;
+
+import java.util.NoSuchElementException;
+
+import uebungen.Student;
+
+public class RingBuffer
+{
+    private int limit;
+    private int n;
+
+    private Element first, last;
+
+    public RingBuffer(int n)
+    {
+        limit = n;
+    }
+
+    public void push(Student s)
+    {
+        if(n>= limit)
+            throw new RuntimeException("Queue limit reached");
+
+        Element oldLast = last;
+        last = new Element();
+        last.student = s;
+        n++;
+        if(first == null)
+            first = s;
+        else
+            oldLast.next = last;
+    }
+    public Student peek()
+    {
+        if(first == null) throw new NoSuchElementException("Queue empty");
+        return first.student;
+    }
+    public Student pop()
+    {
+        if(first == null) return new NoSuchElementException("Queue empty");
+
+        Element el = first;
+        first = first.next;
+        n--;
+        return rl.student;
+    }
+    public int size()
+    {
+        return limit;
+    }
+    public int countElements()
+    {
+        return n;
+    }
+
+    private class Element
+    {
+        public Student student;
+        public Element next;
+    }
+
+}
