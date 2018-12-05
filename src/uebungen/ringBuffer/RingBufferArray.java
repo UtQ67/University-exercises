2632ff213aa69de9f52ce96e81f7131bea108ab0
Utku Pazarci <38797312+UtQ67@users.noreply.github.com>
2018-12-05 22:04:23 +0100

    Dumb modulo

--- src/uebungen/ringBuffer/RingBufferArray.java
+++ src/uebungen/ringBuffer/RingBufferArray.java
@@ -4,9 +4,24 @@ import uebungen.Student;
 
 public class RingBufferArray
 {
+	public static void main(String[] args)
+	{
+		System.out.println(5%5);
+		RingBufferArray test = new RingBufferArray(5);
+		
+		for (int i = 0; i < test.size(); i++)
+		{
+			test.push(new Student("a", "b", i));
+		}
+		test.pop();
+		test.push(new Student("Push test", "", 10));
+		while (test.peek() != null)
+			System.out.println(test.pop().toString());
+	}
+	
 	private Student[] ringBuffer;
 	private int head = 0;
-	private int tail = 0;
+	private int tail = 1;
 	
 	public RingBufferArray(int limit)
 	{
@@ -15,9 +30,8 @@ public class RingBufferArray
 	
 	public void push(Student s)
 	{
-		if(tail + 1== head) throw new  RuntimeException("Queue limit reached");
-		
-		ringBuffer[tail + 1%ringBuffer.length] = s;
+		if(tail == head) throw new  RuntimeException("Queue limit reached");
+		ringBuffer[(tail)%ringBuffer.length] = s;
 		tail++;
 	}
 	
@@ -38,6 +52,6 @@ public class RingBufferArray
 	}
 	public int countElements()
 	{
-		return  tail%ringBuffer.length - head % ringBuffer.length;
+		return  (tail%ringBuffer.length) - (head % ringBuffer.length);
 	}
 }
