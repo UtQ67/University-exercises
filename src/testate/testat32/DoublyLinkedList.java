4cfb6476da180a76427a43bb1e70d4384969648a
Utku Pazarci <38797312+UtQ67@users.noreply.github.com>
2018-12-16 15:59:33 +0100

    .

---
+++ src/testate/testat32/DoublyLinkedList.java
@@ -0,0 +1,249 @@
+package testate.testat32;
+
+public class DoublyLinkedList
+{
+    private Element first, last;
+    private int size;
+
+    public DoublyLinkedList()
+    {
+        first = last = null;
+        size = 0;
+    }
+
+    ///////////////////////
+    public void clear()
+    {
+        first = last = null;
+    }
+    
+    public Object getLast()
+    {
+        if(size == 0) throw new IllegalStateException();
+        return last;
+    }
+    
+    public boolean contains(Object o)
+    {
+        for(Element current = first; current != null; current = current.getSucc())
+        {
+            if(current.getContent() == o)
+                return true;
+        }
+        return false;
+    }
+    
+    public int count(Object o)
+    {
+        int cnt = 0;
+        for(Element current = first; current != null; current = current.getSucc())
+        {
+            if(current.getContent().equals(o))
+                cnt++;
+        }
+        return cnt;
+    }
+    
+    public boolean allEqual()
+    {
+        if(size == 0 || size == 1) return true;
+        
+        for(Element current = first; current != null; current = current.getSucc())
+        {
+            if(!current.getContent().equals(first.getContent()))
+                return false;
+        }
+        return true;
+    }
+    
+    public boolean containsDoubles()
+    {
+        for(Element i = first; i != null; i = i.getSucc())
+        {
+            for (Element j = i.getSucc(); j != null ; j=j.getSucc())
+            {
+                if(i.getContent().equals(j.getContent()))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    public void insert(int n, Object o)
+    {
+        if(size < n) throw new IndexOutOfBoundsException();
+        
+        Element indexN = first;
+        for (int i = 0; i <= n; i++)
+        {
+            indexN = indexN.getSucc();
+        }
+        
+        Element indexNSucc = indexN.getSucc();
+        indexN.disconnectSucc();
+        indexN.connectAsPred(new Element(o));
+        indexN.getSucc().connectAsSucc(indexNSucc);
+        
+        size++;
+    }
+    
+    public void toArray(Object[] arr)
+    {
+        Element current = first;
+    
+        for (int i = 0; i<arr.length; i++)
+        {
+            if(current != null)
+            {
+                arr[i] = current.getContent();
+                current = current.getSucc();
+            }else
+            {
+                arr[i] = null;
+            }
+        }
+    }
+    
+    public DoublyLinkedList flip()
+    {
+        DoublyLinkedList flipped = new DoublyLinkedList();
+        
+        for(Element current = last; current != null; current = current.getPred())
+        {
+            flipped.add(current);
+        }
+        return flipped;
+    }
+    
+    public void remove(int n)
+    {
+        Element current = first;
+        if(n == 0)
+            first = first.getSucc();
+        
+        if (n== size)
+            last = last.getPred();
+        
+        for (int i = 0; i <= n; i++)
+        {
+            current = current.getSucc();
+        }
+        Element succ = current.getSucc();
+        current.disconnectSucc();
+        
+        Element pred = current.getPred();
+        current.disconnectPred();
+        
+        pred.connectAsSucc(succ);
+        
+        size--;
+    }
+    //////////////////////////
+    public int size()
+    {
+        return size;
+    }
+    
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    public void add( Object content ) 
+    {
+        Element e = new Element( content );
+        if ( isEmpty() ) 
+        {
+            first = last = e;
+        }
+        else 
+        {
+            last.connectAsSucc( e );
+            last = e;
+        }
+        size++;
+    }
+
+    public void addFirst( Object content ) 
+    {
+        Element e = new Element( content );
+        if ( isEmpty() ) 
+        {
+            first = last = e;
+        }
+        else 
+        {
+            first.connectAsPred( e );
+            first = e;
+        }
+        size++;
+    }
+
+    public Object getFirst() 
+    {
+        if ( !isEmpty() )
+        {
+            return first.getContent();
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+    }
+
+    public Object get( int index ) 
+    {
+        if ( index >= 0 && index < size )
+        {
+            Element current = first;
+            for ( int i = 0; i < index; i++ )
+            {
+                current = current.getSucc();
+            }
+            return current.getContent();
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+    }
+
+    public Object removeFirst()
+    {
+        if ( !isEmpty() ) 
+        {
+            Object result = first.getContent();
+            if ( first.hasSucc() )
+            {
+                first = first.getSucc();
+                first.disconnectPred();
+            }
+            else
+            {
+                first = last = null;
+            }
+            size--;
+            return result;
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+    }
+
+    public void showAll()
+    {
+        Element current = first;
+        while ( current != null )
+        {
+            System.out.print( current.getContent().toString() );
+            if ( current != last )
+            {
+                System.out.print(", ");
+            }
+            current = current.getSucc();
+        }
+        System.out.println();
+    }
+
+}
